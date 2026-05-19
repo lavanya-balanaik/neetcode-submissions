a85dcf5a7adc5a07a8a534937fa3220c49bd0ec0
lavanya-balanaik <lavanya.balanaik@gmail.com>
2026-05-19 16:08:25 -0700

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.java
@@ -0,0 +1,22 @@
+class LRUCache {
+    Map<Integer, Integer> lru;
+
+    public LRUCache(int capacity) {
+         lru = new LinkedHashMap<Integer, Integer>(capacity, 0.5f, true) {
+            @Override
+            protected boolean removeEldestEntry(
+                    Map.Entry<Integer, Integer> eldest) {
+
+                return size() > capacity;
+            }
+        };
+    }
+    
+    public int get(int key) {
+        return lru.getOrDefault(key, -1);
+    }
+    
+    public void put(int key, int value) {
+        lru.put(key, value);
+    }
+}
